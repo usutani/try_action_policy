02dd1ba8b149997619c0f4f898e548cac185f64d
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-31 09:56:18 +0900

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index show]
+  resources :posts do
+    resources :comments, only: %i[create destroy]
+  end
+  root to: 'users#index'
 end
