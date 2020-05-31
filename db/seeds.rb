580cfce7955ff091ff4674bbe2b0fa567e79017d
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-31 09:59:51 +0900

    User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create! name: 'Administrator', role: 1
+foo = User.create! name: 'Foo'
+bar = User.create! name: 'Bar'
