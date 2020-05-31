4fc5557ec6e5dbf48ab5232e1290894d133130c9
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-31 09:57:25 +0900

    Mod create_table :users

--- db/migrate/20200531005633_create_users.rb
+++ db/migrate/20200531005633_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.integer :role
+      t.string :name, null: false
+      t.integer :role, null: false, default: 0
 
       t.timestamps
     end
