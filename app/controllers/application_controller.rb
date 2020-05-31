cbc8cfce6e81aa04461f9945d00e702ed9f761b5
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-31 10:00:40 +0900

    Pseudo AuthN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_authenticated_user
+
+  private
+
+  def authenticate_user(user_id)
+    if authenticated_user = User.find_by(id: user_id)
+      cookies.encrypted[:user_id] ||= user_id
+      @current_user = authenticated_user
+    else
+      @current_user = nil
+      cookies.delete(:user_id)
+      nil
+    end
+  end
+
+  def ensure_authenticated_user
+    authenticate_user(cookies.encrypted[:user_id]) || redirect_to(root_url)
+  end
 end
