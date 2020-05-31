745aa3a219b4bbf3b8cbaa89ae2b456b5f431cbc
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-31 14:45:35 +0900

    Mod CommentPolicy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,20 +1,6 @@
 class CommentPolicy < ApplicationPolicy
-  # See https://actionpolicy.evilmartians.io/#/writing_policies
-  #
-  # def index?
-  #   true
-  # end
-  #
-  # def update?
-  #   # here we can access our context and record
-  #   user.admin? || (user.id == record.user_id)
-  # end
-
-  # Scoping
-  # See https://actionpolicy.evilmartians.io/#/scoping
-  #
-  # relation_scope do |relation|
-  #   next relation if user.admin?
-  #   relation.where(user: user)
-  # end
+  def destroy?
+    user.admin? || (user.id == record.user_id) ||
+      allowed_to?(:update?, record.post)
+  end
 end
