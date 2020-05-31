a9aacafdf4690e2087f56f3b1481be1b663cc4d3
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-31 10:39:03 +0900

    CommentsController#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create! content: params[:comment][:content],
+                               post: @post, user: @current_user
+  end
 end
